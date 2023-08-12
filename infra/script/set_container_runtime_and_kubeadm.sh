fe662e17a218a3c99ed204e6f953dfd6c2a1b9a5
Young <younghch42@gmail.com>
2023-08-12 23:04:18 +0900

    [FEAT] kublet, kubectl, kubeadm 설치 스크립트 추가

--- infra/script/set_container_runtime_and_kubeadm.sh
+++ infra/script/set_container_runtime_and_kubeadm.sh
@@ -27,3 +27,21 @@ EOF
 
 systemctl restart containerd
 systemctl enable containerd
+
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+systemctl enable --now kubelet
