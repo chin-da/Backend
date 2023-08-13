9835e725dd391b0b21fbb343bf7aac4c85cfbeed
Jin409 <sualng123@gmail.com>
2023-08-13 16:11:13 +0900

    [ADD] Record 추가

---
+++ core/src/main/java/com/core/core/domain/record/Record.java
@@ -0,0 +1,32 @@
+package com.core.core.domain.record;
+
+import com.core.core.domain.exercise.Exercise;
+import com.core.core.domain.user.User;
+import com.core.core.global.entity.BaseEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Record extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "exercise_id")
+    private Exercise exercise;
+
+    @Column(nullable = false)
+    private Integer measurement;
+
+
+}
