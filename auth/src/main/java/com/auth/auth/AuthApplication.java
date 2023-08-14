8bda5d5dd7c3337e8545df3d76f5f8a292e9e14f
Jin409 <sualng123@gmail.com>
2023-08-14 17:54:59 +0900

    [ADD] ComponentScan 관련 세팅

--- auth/src/main/java/com/auth/auth/AuthApplication.java
+++ auth/src/main/java/com/auth/auth/AuthApplication.java
@@ -1,9 +1,13 @@
 package com.auth.auth;
 
+import com.core.core.CoreRoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {
+        CoreRoot.class,
+        AuthApplication.class
+})
 public class AuthApplication {
 
     public static void main(String[] args) {
---
+++ core/src/main/java/com/core/core/CoreRoot.java
@@ -0,0 +1,7 @@
+package com.core.core;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackageClasses = {CoreRoot.class})
+public interface CoreRoot {
+}
