0c6798c5ffe98548065585c43ed3aa7f28d66592
Jin409 <sualng123@gmail.com>
2023-08-14 17:54:35 +0900

    [FIX] BaseEntity 관련 오류 수정

--- core/src/main/java/com/core/core/global/entity/BaseEntity.java
+++ core/src/main/java/com/core/core/global/entity/BaseEntity.java
@@ -6,7 +6,9 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
@@ -23,7 +25,6 @@ public class BaseEntity {
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
-    @Temporal(TemporalType.TIMESTAMP)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_FORMAT,
             timezone = TIME_ZONE)
     private LocalDateTime updatedAt;
