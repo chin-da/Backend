9ab2a4ae880ec9154990439fcee018edb90a2af3
Young <younghch42@gmail.com>
2023-08-15 16:25:20 +0900

    [FEAT] control plane에 pod 배포 허용, alias 추가

--- infra/script/set_control_plane.sh
+++ infra/script/set_control_plane.sh
@@ -1,5 +1,4 @@
 public_ip=$(ip add show eth0 | grep -Po 'inet \K[\d.]+')
-cluster_ip_range=$(echo 10.244.0.0/16)
 kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$public_ip --ignore-preflight-errors 'NumCPU,Mem'
 
 mkdir -p $HOME/.kube
@@ -9,3 +8,9 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 curl -L https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml -o weave-daemonset-k8s.yaml
 vi weave-daemonset-k8s.yaml
 kubectl apply -f weave-daemonset-k8s.yaml
+
+master_node=$(kubectl get nodes | grep control-plane | awk '{print $1}')
+kubectl taint node $master_node node-role.kubernetes.io/control-plane:NoSchedule-
+
+echo 'alias k=kubectl' >> ~/.bash_aliases
+source ~/.bash_aliases
