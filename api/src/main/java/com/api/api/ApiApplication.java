d050e4a09c019ca432c951dc5c9562dcd03ed52f
Jin409 <sualng123@gmail.com>
2023-08-22 17:12:48 +0900

    [ADD] AuthRoot, CommonRoot 추가 및 이에 따른 ApiApplication 수정

--- api/src/main/java/com/api/api/ApiApplication.java
+++ api/src/main/java/com/api/api/ApiApplication.java
@@ -1,9 +1,15 @@
 package com.api.api;
 
+import com.auth.auth.AuthRoot;
+import com.core.core.CoreRoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {
+        CoreRoot.class,
+        AuthRoot.class,
+        ApiApplication.class
+})
 public class ApiApplication {
 
     public static void main(String[] args) {
---
+++ common/src/main/java/com/common/common/config/CommonRoot.java
@@ -0,0 +1,7 @@
+package com.common.common.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackageClasses = {CommonRoot.class})
+public interface CommonRoot {
+}
