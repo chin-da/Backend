7f0d1de5e914737e178bf6787b09bb5ce44c0cae
Jin409 <sualng123@gmail.com>
2023-08-22 17:58:29 +0900

    [DEL] 카카오 소셜 로그인 시 리프레시 토큰을 응답에서 제거

--- auth/src/main/java/com/auth/auth/dto/KakaoTokenResponse.java
+++ auth/src/main/java/com/auth/auth/dto/KakaoTokenResponse.java
@@ -1,5 +1,6 @@
 package com.auth.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AccessLevel;
@@ -8,15 +9,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KakaoTokenResponse {
 
     private String accessToken;
-    private String refreshToken;
-
-    public static KakaoTokenResponse of(String accessToken, String refreshToken) {
-        return new KakaoTokenResponse(accessToken, refreshToken);
-    }
 }
