24bc75daee0dd66d2e314f6e3dbadea20320cd0a
Jin409 <sualng123@gmail.com>
2023-08-22 23:55:18 +0900

    [REFACTOR] OAuthService 에서 UserRepository 를 주입받도록 수정

--- core/src/main/java/com/core/core/domain/user/service/UserService.java
+++ core/src/main/java/com/core/core/domain/user/service/UserService.java
@@ -1,20 +1,9 @@
 package com.core.core.domain.user.service;
 
-import com.core.core.domain.user.User;
-import com.core.core.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-
-    private final UserRepository userRepository;
-
-    public Optional<User> findUserWithSocialId(final Long socialId) {
-        Optional<User> user = userRepository.findUserBySocialId(socialId);
-        return user;
-    }
 }
