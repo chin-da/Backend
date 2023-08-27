06984cc9949ea919eb498481c6c0ce12b010ce3d
Young <younghch42@gmail.com>
2023-08-27 23:22:15 +0900

    [FIX] 클러스터 생성은 메뉴얼로 유지

--- infra/script/init_node.sh
+++ infra/script/init_node.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 yum update -y
 yum install git -y
 
@@ -8,8 +9,9 @@ git init
 git remote add -f origin https://github.com/chin-da/Backend.git
 git config core.sparseCheckout true
 echo "infra" >>.git/info/sparse-checkout
+# TODO: change branch name to main before merge
 git pull origin fix/#18-change-network-plugin
 
 cd ~/chinda/infra/script
 chmod +x *.sh
-sh set_container_runtime_and_kubeadm.sh
+sh ./set_container_runtime_and_kubeadm.sh
