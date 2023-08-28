18c2414b55b7a83db625d23754866f54509ae9e6
Jin409 <sualng123@gmail.com>
2023-08-28 16:50:24 +0900

    [ADD] UNAUTHORIZED_EXCEPTION 관련 에러 코드 추가

--- common/src/main/java/com/common/common/exception/ErrorCode.java
+++ common/src/main/java/com/common/common/exception/ErrorCode.java
@@ -10,6 +10,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     /**
+     * 401 UNAUTHORIZED
+     */
+    UNAUTHORIZED_EXCEPTION(HttpStatus.UNAUTHORIZED, "권한이 없는 사용자입니다."),
+
+    /**
      * 404 NOT FOUND
      */
     NOT_FOUND_USER_EXCEPTION(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
