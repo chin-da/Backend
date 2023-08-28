f5920e49cd10112c44afbf21b5e33bc356bc7a4b
Jin409 <sualng123@gmail.com>
2023-08-28 15:45:32 +0900

    [ADD] 에러 응답 형태 추가

---
+++ common/src/main/java/com/common/common/exception/dto/ErrorResponse.java
@@ -0,0 +1,24 @@
+package com.common.common.exception.dto;
+
+import com.common.common.exception.ErrorCode;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ErrorResponse<T> {
+
+    private final int code;
+    private final String message;
+
+    public static ErrorResponse error(ErrorCode error) {
+        return new ErrorResponse<>(error.getHttpStatusCode(), error.getMessage());
+    }
+
+    public static ErrorResponse error(ErrorCode error, String message) {
+        return new ErrorResponse<>(error.getHttpStatusCode(), message);
+    }
+}
