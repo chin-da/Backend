10bd40ace6baf3c4cd150b0c0a52dd03b158cc79
Jin409 <sualng123@gmail.com>
2023-08-28 15:46:21 +0900

    [ADD] api 모듈에 common 모듈 관련 설정 추가

--- api/src/main/java/com/api/api/ApiApplication.java
+++ api/src/main/java/com/api/api/ApiApplication.java
@@ -1,6 +1,7 @@
 package com.api.api;
 
 import com.auth.auth.AuthRoot;
+import com.common.common.config.CommonRoot;
 import com.core.core.CoreRoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackageClasses = {
         CoreRoot.class,
         AuthRoot.class,
+        CommonRoot.class,
         ApiApplication.class
 })
 public class ApiApplication {
