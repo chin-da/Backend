7dfabf83355b3ddb1b4aca774c9e9f90e2538f1f
Jin409 <sualng123@gmail.com>
2023-09-01 16:36:35 +0900

    [TEST] 테스트 환경에서 스프링부트 서버가 필요하지 않기 때문에 삭제

--- auth/src/test/java/com/auth/auth/service/JwtProviderTest.java
+++ auth/src/test/java/com/auth/auth/service/JwtProviderTest.java
@@ -3,24 +3,20 @@ package com.auth.auth.service;
 import com.auth.auth.TestConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
 @ActiveProfiles({"local"})
 @ContextConfiguration(classes = TestConfiguration.class)
 public class JwtProviderTest {
 
-    @Autowired
-    private JwtProvider jwtProvider;
+    private final JwtProvider jwtProvider = new JwtProvider();
 
     private final Long userId;
 
-    public JwtProviderTest() {
+    public JwtProviderTest() throws Exception {
         this.userId = 1L;
     }
 
