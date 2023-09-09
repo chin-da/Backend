be5f748bbe422a6eda44145fe8242290f8eb122d
Young <younghch42@gmail.com>
2023-09-09 21:43:10 +0900

    [REFACTOR] 토큰 발급 기능 변경된 클래스에 맞게 변경

--- app/user/src/main/java/com/chinda/user/application/AccountService.java
+++ app/user/src/main/java/com/chinda/user/application/AccountService.java
@@ -1,5 +1,6 @@
 package com.chinda.user.application;
 
+import com.chinda.user.domain.iam.AccessToken;
 import com.chinda.user_shared_kernel.model.Platform;
 import com.chinda.user.domain.iam.KakaoOAuthRequester;
 import com.chinda.user.domain.iam.OAuthAgreedUser;
@@ -7,6 +8,7 @@ import com.chinda.user.domain.iam.OAuthRequester;
 import com.chinda.user.domain.user.UserRepository;
 import com.chinda.user_shared_kernel.model.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -18,27 +20,36 @@ public class AccountService {
     private final KakaoOAuthRequester kakaoOAuthRequester;
     private final UserRepository userRepository;
 
-    public void login(final String platformName, final String authCode) {
+    @Value("${jwt.public-key}")
+    private final String publicKey;
+    @Value("${jwt.private-key}")
+    private final String privateKey;
+    private static final Long jwtExpiration = 120 * 60 * 1000L;
+
+
+    public AccessToken login(final String platformName, final String authCode) {
         Platform platform = Platform.valueOf(platformName);
         OAuthRequester oAuthRequester = getOAuthRequester(platform);
         OAuthAgreedUser oAuthAgreedUser = oAuthRequester.getOAuthAgreedUser(authCode);
-        if(isRegistered(oAuthAgreedUser.getSocialId())) {
-            // TODO : 토큰 반환 기능 구현
+
+        Optional<User> user = userRepository.findUserBySocialId(oAuthAgreedUser.getSocialId());
+        if(user.isPresent()) {
+            User registeredUser = user.get();
+            return new AccessToken(registeredUser.getId(), jwtExpiration, privateKey);
         } else {
-            throw new RuntimeException("Not Registered User");
+            throw new RuntimeException("Not a Registered User");
         }
     }
 
     private OAuthRequester getOAuthRequester(final Platform platform) {
-        switch (platform) {
-            case KAKAO:
-                return kakaoOAuthRequester;
-            default:
-                throw new RuntimeException("Not Supported Platform");
-        }
+        return switch (platform) {
+            case KAKAO -> kakaoOAuthRequester;
+            case GOOGLE -> throw new UnsupportedOperationException();
+            default -> throw new RuntimeException("Not a Supported Platform");
+        };
     }
     public boolean isRegistered(final Long socialId) {
-        Optional<User> user = userRepository.findBySocialId(socialId);
+        Optional<User> user = userRepository.findUserBySocialId(socialId);
         return user.isPresent();
     }
 
