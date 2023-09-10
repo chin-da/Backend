ffbe2b30ba4cd9ad5248d930f7e9b48d4cc14ddc
Young <younghch42@gmail.com>
2023-09-10 18:49:50 +0900

    [FIX] 잘못된 스크립트 변수명, 값 수정

--- infra/script/local/create_local_key_and_export_to_env.sh
+++ infra/script/local/create_local_key_and_export_to_env.sh
@@ -1,5 +1,5 @@
 openssl genrsa -out chinda_private.pem 2048
 openssl rsa -in chinda_private.pem -pubout -out chinda_public.pem
 
-echo export jwt-public-key-pem=$(cat chinda_public.pem) >> ~/.zshenv
-echo export jwt-private-key-pem=$(cat chinda_private.pem) >> ~/.zshenv
+echo "export JWT_PUBLIC_KEY_PEM='$(cat chinda_public.pem)'" >> ~/.zshrc
+echo "export JWT_PRIVATE_KEY_PEM='$(cat chinda_private.pem)'" >> ~/.zshrc
