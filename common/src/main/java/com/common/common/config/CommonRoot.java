4bf78cae091b7fc9d25cc2bd9c7f7c99e62e8a9c
Young <younghch42@gmail.com>
2023-09-16 22:22:55 +0900

    [DELTE] 이관 완료 모듈 삭제

--- common/src/main/java/com/common/common/config/CommonRoot.java
+++
@@ -1,7 +0,0 @@
-package com.common.common.config;
-
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan(basePackageClasses = {CommonRoot.class})
-public interface CommonRoot {
-}
