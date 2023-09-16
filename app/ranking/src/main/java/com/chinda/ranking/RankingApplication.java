50ea743eed8041585f517b2372e0b9d766083574
Young <younghch42@gmail.com>
2023-09-16 22:36:53 +0900

    [FIX] 랭킹어플리케이션에 누락된 컴포넌트, 엔티티 스캔 추가

--- app/ranking/src/main/java/com/chinda/ranking/RankingApplication.java
+++ app/ranking/src/main/java/com/chinda/ranking/RankingApplication.java
@@ -2,8 +2,10 @@ package com.chinda.ranking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.chinda.ranking", "com.chinda.common"})
+@EntityScan("com.chinda.iam_shared_kernel.*")
 public class RankingApplication {
 
 	public static void main(String[] args) {
