c18c8eea83ca67dd614b58e725fb08e653bda2d9
Jin409 <sualng123@gmail.com>
2023-09-20 14:03:56 +0900

    [FEAT] 닉네임이 존재하면 예외를 반환하는 기능 구현

--- app/iam/src/main/java/com/chinda/iam/application/IdentityService.java
+++ app/iam/src/main/java/com/chinda/iam/application/IdentityService.java
@@ -1,11 +1,26 @@
 package com.chinda.iam.application;
 
+import com.chinda.common.exception.MessageConstants;
+import com.chinda.iam.application.exceptions.DuplicateNicknameException;
+import com.chinda.iam.domain.identity.UserRepository;
+import com.chinda.iam.web.iam.dto.request.NicknameValidationRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class IdentityService {
 
+    private final UserRepository userRepository;
+
+    public void validateNickname(final NicknameValidationRequestDto nicknameValidationRequestDto) {
+        String nickname = nicknameValidationRequestDto.getNickname();
+
+        if (userRepository.existsByNickname(nickname)) {
+            throw new DuplicateNicknameException(MessageConstants.CONFLICT_NICKNAME.getMessage());
+        }
+    }
 
 }
