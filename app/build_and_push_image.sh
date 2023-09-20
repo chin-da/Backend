a3425ccedb9c353c538dda02e38944013fb837ca
Young <younghch42@gmail.com>
2023-09-20 15:09:39 +0900

    [FEAT] 로컬 이미지 빌드 및 업로드 스크립트 추가

---
+++ app/build_and_push_image.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+APP_DIR=$(dirname $0)
+cd "$APP_DIR"
+
+IMAGE_DIR="$1"
+IMAGE_NAME="chinda_$IMAGE_DIR"
+IMAGE_TAG=$(git rev-parse --short HEAD)
+ECR_REPO="627145993949.dkr.ecr.ap-northeast-2.amazonaws.com"
+
+docker build -t $IMAGE_NAME:$IMAGE_TAG -f ./$IMAGE_DIR/Dockerfile .
+echo "Successfully built image: $IMAGE_NAME:$IMAGE_TAG"
+
+if [ "$2" = "push" ]; then
+    aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin $ECR_REPO
+    docker tag "$IMAGE_NAME:$IMAGE_TAG" "$ECR_REPO/$IMAGE_NAME:$IMAGE_TAG"
+    docker push "$ECR_REPO/$IMAGE_NAME:$IMAGE_TAG"
+    echo "Successfully pushed to ECR: $IMAGE_NAME:$IMAGE_TAG"
+fi
