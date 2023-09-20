092734ae3f6db348fe6c46e0d0222eaa248562f7
Jin409 <sualng123@gmail.com>
2023-09-20 12:53:09 +0900

    [ADD] 반복적으로 사용되는 유효성 검사 메시지 추가

---
+++ app/common/src/main/java/com/chinda/common/ValidatorMessage.java
@@ -0,0 +1,11 @@
+package com.chinda.common;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ValidatorMessage {
+
+    public static final String EMPTY_MESSAGE = "비어 있는 항목을 입력해주세요";
+
+}
