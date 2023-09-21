6285b3e15950bcb9f298b756d33e4e2fd8d9ff5b
Jin409 <sualng123@gmail.com>
2023-09-21 16:25:15 +0900

    [DEL] 사용되지 않는 import 문 삭제

--- app/iam/src/main/java/com/chinda/iam/IAMApplication.java
+++ app/iam/src/main/java/com/chinda/iam/IAMApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"com.chinda.iam", "com.chinda.common"})
 @EnableFeignClients("com.chinda.iam")
@@ -14,8 +13,8 @@ import org.springframework.context.annotation.ComponentScan;
 @EntityScan("com.chinda.iam_shared_kernel.*")
 public class IAMApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IAMApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IAMApplication.class, args);
+    }
 
 }
