061fa1965916d9acd00a857ff871322cb81ce514
Jin409 <sualng123@gmail.com>
2023-09-21 16:15:02 +0900

    [FEAT] 운동 기록 기능을 제외한 회원가입 기능 구현

--- app/iam/src/main/java/com/chinda/iam/application/IdentityService.java
+++ app/iam/src/main/java/com/chinda/iam/application/IdentityService.java
@@ -4,6 +4,8 @@ import com.chinda.common.exception.MessageConstants;
 import com.chinda.iam.application.exceptions.DuplicateNicknameException;
 import com.chinda.iam.domain.identity.UserRepository;
 import com.chinda.iam.web.iam.dto.request.NicknameValidationRequestDto;
+import com.chinda.iam.web.iam.dto.request.SignUpRequestDto;
+import com.chinda.iam_shared_kernel.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +16,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class IdentityService {
 
     private final UserRepository userRepository;
+
+    public void register(final SignUpRequestDto signUpRequestDto) {
+        User user = signUpRequestDto.toUser();
+        userRepository.save(user);
+        // TODO: Record 저장 기능 구현 이후 추가
+    }
 
     public void validateNickname(final NicknameValidationRequestDto nicknameValidationRequestDto) {
         String nickname = nicknameValidationRequestDto.getNickname();
--- app/iam/src/main/java/com/chinda/iam/web/iam/AuthController.java
+++ app/iam/src/main/java/com/chinda/iam/web/iam/AuthController.java
@@ -5,6 +5,7 @@ import com.chinda.iam.application.IdentityService;
 import com.chinda.iam.domain.access.AccessToken;
 import com.chinda.iam.web.iam.dto.request.LoginVo;
 import com.chinda.iam.web.iam.dto.request.NicknameValidationRequestDto;
+import com.chinda.iam.web.iam.dto.request.SignUpRequestDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +31,12 @@ public class AuthController {
     @PostMapping("/validations/nickname")
     public ResponseEntity<Void> validateNickname(@RequestBody @Valid final NicknameValidationRequestDto nicknameValidationRequestDto) {
         identityService.validateNickname(nicknameValidationRequestDto);
+        return ResponseEntity.ok(null);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody @Valid final SignUpRequestDto signUpRequestDto) {
+        identityService.register(signUpRequestDto);
         return ResponseEntity.ok(null);
     }
 
