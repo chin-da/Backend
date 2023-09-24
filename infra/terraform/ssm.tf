ccbe22d7ad3ada8469f27b297c99bb54fe7b486b
Young <younghch42@gmail.com>
2023-09-24 10:00:06 +0900

    [ADD] 설정 변수 ssm에 저장

---
+++ infra/terraform/ssm.tf
@@ -0,0 +1,60 @@
+resource "aws_ssm_parameter" "db_url" {
+  name      = "/chinda/db/url"
+  type      = "String"
+  value     = aws_db_instance.chinda_mysql.address
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "db_username" {
+  name      = "/chinda/db/username"
+  type      = "SecureString"
+  value     = var.db_username
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "db_password" {
+  name      = "/chinda/db/password"
+  type      = "SecureString"
+  value     = var.db_password
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "chinda_public_key" {
+  name      = "/chinda/ssl/public_key"
+  type      = "String"
+  value     = file("./keys/chinda_public.pem")
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "chinda_private_key" {
+  name      = "/chinda/ssl/private_key"
+  type      = "SecureString"
+  value     = file("./keys/chinda_private.pem")
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "chinda_kakao_client_id" {
+  name      = "/chinda/oauth/kakao_client_id"
+  type      = "String"
+  value     = var.chinda_kakao_client_id
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "chinda_redirect_uri" {
+  name      = "/chinda/oauth/redirect_uri"
+  type      = "String"
+  value     = var.chinda_redirect_uri
+  overwrite = true
+}
+
+variable "chinda_kakao_client_id" {
+  description = "kakao client id"
+  type        = string
+  sensitive   = false
+}
+
+variable "chinda_redirect_uri" {
+  description = "oauth redirect uri"
+  type        = string
+  sensitive   = false
+}
