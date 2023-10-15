9efda504e4945bb30b2d2b6da68acf3f5dcbc47c
Young <younghch42@gmail.com>
2023-10-15 14:41:39 +0900

    [FIX] 새로 푸시된 이미지가 latest태그를 가지도록 수정

--- app/build_and_push_image.sh
+++ app/build_and_push_image.sh
@@ -12,17 +12,8 @@ ECR_REPO="627145993949.dkr.ecr.ap-northeast-2.amazonaws.com"
 docker buildx build . \
     --platform linux/amd64,linux/arm64 \
     -t $ECR_REPO/$IMAGE_NAME:$IMAGE_TAG \
+    -t $ECR_REPO/$IMAGE_NAME:latest \
     -f ./$IMAGE_DIR/Dockerfile \
     --push
 
-echo "Successfully built image: $IMAGE_NAME:$IMAGE_TAG"
-
-
-if [ "$2" = "push" ]; then
-    aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin $ECR_REPO
-    docker tag "$IMAGE_NAME:$IMAGE_TAG" "$ECR_REPO/$IMAGE_NAME:$IMAGE_TAG"
-    docker push "$ECR_REPO/$IMAGE_NAME:$IMAGE_TAG"
-    docker tag "$IMAGE_NAME:$IMAGE_TAG" "$ECR_REPO/$IMAGE_NAME:latest"
-    docker push "$ECR_REPO/$IMAGE_NAME:latest"
-    echo "Successfully pushed to ECR: $IMAGE_NAME:$IMAGE_TAG"
-fi
+echo "Successfully push image: $IMAGE_NAME:$IMAGE_TAG"
