67b4e8f252ef10ba1614f7ae7acbeca134aff62a
Young <younghch42@gmail.com>
2023-10-15 21:25:54 +0900

    [FIX] 잘못된 url수정

--- infra/k8s/db_secret.sh
+++ infra/k8s/db_secret.sh
@@ -1,4 +1,4 @@
 kubectl create secret generic db-secret \
-  --from-literal=url=$(aws ssm get-parameter --name "/chinda/db/url" --query Parameter.Value --output text --region ap-northeast-2):3306/chinda \
+  --from-literal=url=$(aws ssm get-parameter --name "/chinda/db/url" --query Parameter.Value --output text --region ap-northeast-2)/chinda \
   --from-literal=username=$(aws ssm get-parameter --name "/chinda/db/username" --with-decryption --query Parameter.Value --output text --region ap-northeast-2) \
   --from-literal=password=$(aws ssm get-parameter --name "/chinda/db/password" --with-decryption --query Parameter.Value --output text --region ap-northeast-2) 
