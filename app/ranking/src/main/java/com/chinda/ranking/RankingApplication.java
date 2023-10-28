a2477111247e8ad67bd1e6c9bdafe076c632c832
Young <younghch42@gmail.com>
2023-10-28 16:13:05 +0900

    [BUGFIX] entity scan 대상되지 않는 문제 해결

--- app/ranking/src/main/java/com/chinda/ranking/RankingApplication.java
+++ app/ranking/src/main/java/com/chinda/ranking/RankingApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = {"com.chinda.ranking", "com.chinda.common"})
-@EntityScan("com.chinda.iam_shared_kernel.*")
+@SpringBootApplication(scanBasePackages = {"com.chinda.ranking", "com.chinda.common", "com.chinda.iam_shared_kernel"})
+@EntityScan({"com.chinda.iam_shared_kernel.*", "com.chinda.ranking.*"})
 public class RankingApplication {
 
 	public static void main(String[] args) {
